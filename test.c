7dbed0ffc2f4dd26d504781cc3408335de2fcbe3
Satish Patel <tosatishpatel@gmail.com>
2021-07-12 15:43:01 +0530

    Added test function for addition

--- test.c
+++ test.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+void test_func(int a, int b) {
+    int ans = a + b;
+    printf("\n Answer:%d",ans);
+}
 int main() {
     printf("\n Hello World");
     for(int i = 0; i < 500; i++) {
         printf("\n test loop:%d",i);
     }
+    test_func(23,40);
     printf("\n End of application");
     return 0;
 }
